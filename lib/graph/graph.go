22f60dbcf39f163cd53c227fb74107df8fc4fc17
Hulya Pamukcu Crowell <pamukcuh@gmail.com>
2019-12-28 15:03:26 -0800

    Add itinerary example for graph

--- lib/graph/graph.go
+++ lib/graph/graph.go
@@ -6,7 +6,7 @@ type Metadata map[string]interface{}
 // Graph with Nodes and metadata
 type Graph struct {
 	Nodes map[string]*Node
-	MData *Metadata
+	MData Metadata
 }
 
 // Node with name, object holding the data, connections in and out, and metadata
@@ -17,39 +17,33 @@ type Node struct {
 	Data interface{}
 
 	// Outgoing edges Target node.Name is the key, current node is the source
-	EdgesOut map[string]*Edge
+	EdgesOut map[string]Edge
 
 	// Source node.Name is the key, current node is the target
-	EdgesIn map[string]*Edge
+	EdgesIn map[string]Edge
 
 	// This is free form map to set values while traversing graph, e.g. "isVisited = true"
 	// Normally would be used for more advanced scenarios
-	MData *Metadata
+	MData Metadata
 }
 
 // Edge from source to target with metadata
 type Edge struct {
 	Source   *Node
 	Target   *Node
-	Metadata *Metadata
+	Metadata Metadata
 }
 
 func NewNode(name string, data interface{}) *Node {
 	node := Node{
 		Name:     name,
 		Data:     data,
-		MData:    &Metadata{},
-		EdgesIn:  make(map[string]*Edge),
-		EdgesOut: make(map[string]*Edge),
+		MData:    Metadata{},
+		EdgesIn:  make(map[string]Edge),
+		EdgesOut: make(map[string]Edge),
 	}
 
 	return &node
-}
-
-func NewNodeWithMetadata(name string, data interface{}, mData *Metadata) *Node {
-	node := NewNode(name, data)
-	node.MData = mData
-	return node
 }
 
 func NewGraph() *Graph {
@@ -58,46 +52,30 @@ func NewGraph() *Graph {
 	return &g
 }
 
-func NewGraphWithMetadata(mData *Metadata) *Graph {
-	g := NewGraph()
-	g.MData = mData
-	return g
-}
-
 // Add relation to the graph, directional from source node to target node
 func (g *Graph) Add(source, target *Node) {
-	g.AddWithMetadata(source, target, &Metadata{})
-}
-
-// Add relation to the graph, directional from source node to target node with metadata
-func (g *Graph) AddWithMetadata(source, target *Node, mData *Metadata) {
 	g.Nodes[source.Name] = source
 
 	if target != nil {
 		g.Nodes[target.Name] = target
-		g.addEdge(source, target, mData)
+		g.addEdge(source, target)
 	}
 }
 
 // Add relation to the graph, bidirectional with node1 and node2, each direction with its own metadata
 func (g *Graph) AddBidirectional(node1, node2 *Node) {
-	g.AddBidirectionalWithMetadata(node1, node2, &Metadata{}, &Metadata{})
-}
-
-// Add relation to the graph, bidirectional with node1 and node2, each direction with its own metadata
-func (g *Graph) AddBidirectionalWithMetadata(node1, node2 *Node, mData12, mData21 *Metadata) {
 	g.Nodes[node1.Name] = node1
 	g.Nodes[node2.Name] = node2
-	g.addEdge(node1, node2, mData12)
-	g.addEdge(node2, node1, mData21)
+	g.addEdge(node1, node2)
+	g.addEdge(node2, node1)
 }
 
-func (g *Graph) addEdge(from, to *Node, mData *Metadata) {
+func (g *Graph) addEdge(from, to *Node) {
 	if to != nil && from != nil {
-		v := &Edge{
+		v := Edge{
 			Source:   from,
 			Target:   to,
-			Metadata: mData,
+			Metadata: Metadata{},
 		}
 
 		g.Nodes[from.Name].EdgesOut[to.Name] = v
