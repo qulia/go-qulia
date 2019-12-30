5355e924938ac8791d32f0aaa2a1d8a8a4369652
Hulya Pamukcu Crowell <pamukcuh@gmail.com>
2019-12-29 16:53:39 -0800

    Add more test bst

--- lib/tree/bst_test.go
+++ lib/tree/bst_test.go
@@ -17,4 +17,18 @@ func TestBSTBasic(t *testing.T) {
 
 	res := bst.ToSlice()
 	assert.Equal(t, []interface{}{1, 3, 4, 6, 7, 8, 10, 13, 14}, res)
+
+	assert.Equal(t, 8, bst.Search(8).Data.(int))
+	assert.Equal(t, 1, bst.Search(1).Data.(int))
+	assert.Equal(t, 14, bst.Search(14).Data.(int))
+}
+
+func TestBSTEmpty(t *testing.T) {
+	bst := tree.NewBST(lib.IntCompFunc)
+	res := bst.ToSlice()
+	var exp []interface{}
+	assert.Equal(t, exp, res)
+
+	assert.Nil(t, bst.Search(2))
+	assert.Nil(t, bst.Search(nil))
 }
