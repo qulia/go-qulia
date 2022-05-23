f07a87f67786782331b062fcf4e1505d8f032aca
Hulya Pamukcu Crowell <hulyapac@Hulyas-MBP.home>
2022-05-23 15:14:32 -0700

    update heap

--- lib/common.go
+++ lib/common.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"github.com/qulia/go-qulia/lib/hash"
+	"golang.org/x/exp/constraints"
 )
 
 // Metadata to append properties,tags to Graph, Node, Edge, etc
@@ -14,6 +15,18 @@ type Metadata map[string]interface{}
 // function takes two elements and returns positive value if first > second,
 // negative value if first < second, 0 otherwise
 type OrderFunc func(first, second interface{}) int
+
+type Lesser[T any] interface {
+	Less(T) bool
+}
+
+type DefaultLesser[T constraints.Ordered] struct {
+	Val T
+}
+
+func (dc DefaultLesser[T]) Less(other DefaultLesser[T]) bool {
+	return dc.Val < other.Val
+}
 
 type Keyable[K comparable] interface {
 	Key() K
--- lib/heap/doc.go
+++ lib/heap/doc.go
@@ -2,7 +2,7 @@
 Package heap provides easy to use MinHeap and MaxHeap implementations
 https://en.wikipedia.org/wiki/Heap_(data_structure)
 
-Interface methods Insert,Extract,IsEmpty,Size are the ways to interact with heap data structure.
-The Examples, file example_priority_queue_test.go, include a job priority queue implementation using MinHeap
+Interface methods Insert, Extract, IsEmpty, Size are the ways to interact with heap data structure.
+The Examples, file example_priority_queue_test.go, demonstrates a job priority queue
 */
 package heap
