4a593da409d192620a79c93e48148d12694ca7f9
Hulya Pamukcu Crowell <pamukcuh@gmail.com>
2023-02-28 13:44:15 -0800

    Make BIT 0 indexed interface and update method name

--- lib/tree/bit.go
+++ lib/tree/bit.go
@@ -1,33 +1,36 @@
 package tree
 
 type BinaryIndexTreeInterface interface {
-	//add "val" at index "x"
+	// add "val" at index "x"
+	// 0 indexed bounded by the size of the index tree
 	Update(x, val int)
 
-	//returns the sum of first x elements
-	Query(x int) int
+	// returns the sum of elements up to and including index x
+	Sum(x int) int
 }
 
 type BinaryIndexTree struct {
-	size int
-	buf  []int
+	buf []int
 }
 
 func (bit BinaryIndexTree) Update(x, val int) {
-	for ; x <= bit.size; x += x & -x {
+	x += 1
+	for ; x > 0 && x < len(bit.buf); x += x & -x {
 		bit.buf[x] += val
 	}
 }
 
-func (bit BinaryIndexTree) Query(x int) int {
+func (bit BinaryIndexTree) Sum(x int) int {
+	x += 1
 	sum := 0
-	for ; x > 0; x -= x & -x {
+	for ; x > 0 && x < len(bit.buf); x -= x & -x {
 		sum += bit.buf[x]
 	}
 	return sum
 }
 
 func NewBinaryIndexTree(size int) BinaryIndexTreeInterface {
-	bit := BinaryIndexTree{size: size + 1, buf: make([]int, size+1)}
+	// bit buffer is 1 indexed
+	bit := BinaryIndexTree{buf: make([]int, size+1)}
 	return &bit
 }
