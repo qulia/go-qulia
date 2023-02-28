c64a481c101871e0fe76658f5888145042dca83d
Hulya Pamukcu Crowell <pamukcuh@gmail.com>
2023-02-28 13:57:39 -0800

    Change update to set instead of add

--- lib/tree/bit_test.go
+++ lib/tree/bit_test.go
@@ -9,6 +9,7 @@ import (
 func TestBinaryIndexTree_Query(t *testing.T) {
 	bit := NewBinaryIndexTree(10)
 	for i := 1; i <= 10; i++ {
+		// 0:1, 1:2, 2:3, ...
 		bit.Update(i-1, i)
 	}
 
@@ -19,4 +20,7 @@ func TestBinaryIndexTree_Query(t *testing.T) {
 	for i := 1; i <= 10; i++ {
 		assert.Equal(t, i, bit.Sum(i-1)-bit.Sum(i-2))
 	}
+
+	bit.Update(5, 8)                     // update 6 to 8
+	assert.Equal(t, 7*8/2+2, bit.Sum(6)) // (sum 1 to 7) + 2
 }
